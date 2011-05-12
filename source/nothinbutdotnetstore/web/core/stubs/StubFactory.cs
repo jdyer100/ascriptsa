d870ba395e4065d28e3ef98ae0227ae873597f9f
Jeff Dyer <jeffdyer100@email.com>
2011-05-12 17:27:15 -0400

    Pushing new changes

---
+++ source/nothinbutdotnetstore/web/core/stubs/StubFactory.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace nothinbutdotnetstore.web.core.stubs
+{
+    public static class StubFactory<T> where T: new()
+    {
+        public static T Create()
+        {
+            return new T();
+        }
+    }
+}
